c076a0f908a5157dc0bf6c44a30a4a142caf2002
Artem Kasperovich <artem.kas.cfc@gmail.com>
2022-11-24 14:32:01 +0300

    22 tests are added

--- cypress/e2e/registration-form.cy.js
+++ cypress/e2e/registration-form.cy.js
@@ -10,14 +10,14 @@ describe("Registration form", () => {
         randomSurname = random.nameSurname(2, 128),
         randomEmail = random.email(4, 64, 2, 32, 2, 16),
         randomPassword = random.password(8, 16),
-        minPlusOneAllPartsE = random.email(5, 5, 3, 3, 3, 3),
-        maxMinusOneAllPartsE = random.email(63, 63, 31, 31, 15, 15)
+        minPlusOneAllLengthE = random.email(5, 5, 3, 3, 3, 3),
+        maxMinusOneAllLengrhE = random.email(63, 63, 31, 31, 15, 15)
   
   beforeEach(() => {
     cy.visit('/')
   })     
   
-  it.only("Check tab, title and labels of the page", () => {
+  it.skip("Check tab, title and labels of the page", () => {
     cy.url().should('eql', 'http://itcareer.pythonanywhere.com/');
     cy.title().should('eql', 'Demo Form');
     cy.get('h2').invoke('text').should('eql','ITCareerToday web form')
@@ -25,27 +25,30 @@ describe("Registration form", () => {
       const lables = ['Name:', 'Surname:', 'Email:', 'Password:']
       expect($el.text()).includes(lables[ind])
     })
+    //Label suname instead of surname
   });
 
-  it("Check registration by filling in the fields with min valid values.", () => {
+  it.skip("Check registration by filling in the fields with min valid values.", () => {
     const minName = random.nameSurname(2, 2),
           minSurname = random.nameSurname(2, 2),
           minEmail = random.email(4, 4, 2, 2, 2, 2),
           minPassword = random.password(8, 8)
           
     regForm.fillInValid(minName, minSurname, minEmail, minPassword);
+    //Name field requires minimum 4 symbols;
     });
 
-  it("Check registration by filling in the fields with max valid values.", () => {
+  it.skip("Check registration by filling in the fields with max valid values.", () => {
     const maxName = random.nameSurname(128, 128),
           maxSurname = random.nameSurname(128, 128),
           maxEmail = random.email(64, 64, 32, 32, 16, 16),
           maxPassword = random.password(16, 16)
     
     regForm.fillInValid(maxName, maxSurname, maxEmail, maxPassword);
+    //Name field requires maximum 25 symbols;
   });
 
-  it("Check registration by filling in the fields with min+1 valid values.", () => {
+  it.skip("Check registration by filling in the fields with min+1 valid values.", () => {
     const minPlusOneN = random.nameSurname(3, 3),
           minPlusOneS = random.nameSurname(3, 3),
           minPlusOneE = random.email(5, 5, 2, 2, 2, 2),
@@ -54,7 +57,7 @@ describe("Registration form", () => {
     regForm.fillInValid(minPlusOneN, minPlusOneS, minPlusOneE, minPlusOneP);
   });
 
-  it("Check registration by filling in the fields with max-1 valid values.", () => {
+  it.skip("Check registration by filling in the fields with max-1 valid values.", () => {
     const maxMinusOneN = random.nameSurname(127, 127),
           maxMinusOneS = random.nameSurname(127, 127),
           maxMinusOneE = random.email(63, 63, 32, 32, 16, 16),
@@ -66,85 +69,90 @@ describe("Registration form", () => {
   it.skip("Check registration by filling in the Name field with min-1 valid values.", () => {
     const minMinusOneN = random.nameSurname(1, 1)
     
-    regForm.fillIn(minMinusOneN, randomSurname, randomEmail, randomPassword);
+    regForm.fillInNotValidLength(minMinusOneN, randomSurname, randomEmail, randomPassword);
+    // Error about NameLength 
   });
 
   it.skip("Check registration by filling in the Name field with max+1 valid values.", () => {
     const maxPlusOneN = random.nameSurname(129, 129)
     
-    regForm.fillIn(maxPlusOneN, randomSurname, randomEmail, randomPassword);
+    regForm.fillInNotValidLength(maxPlusOneN, randomSurname, randomEmail, randomPassword);
   });
 
   it.skip("Check registration by filling in the Surname field with min-1 valid values.", () => {
     const minMinusOneS = random.nameSurname(1, 1)
     
-    regForm.fillIn(randomName, minMinusOneS, randomEmail, randomPassword);
+    regForm.fillInNotValidLength(randomName, minMinusOneS, randomEmail, randomPassword);
+    //Surname field accepts 1 symbol (min -1);
   });
 
   it.skip("Check registration by filling in the Surname field with max+1 valid values.", () => {
     const maxPlusOneS = random.nameSurname(129, 129)
     
-    regForm.fillIn(randomName, maxPlusOneS, randomEmail, randomPassword);
+    regForm.fillInNotValidLength(randomName, maxPlusOneS, randomEmail, randomPassword);
+    //Error about Surname length
+    //Surname field accept 129 symbol (max +1)
   });
 
   it.skip("Check registration by filling in the Email field with min-1 valid values before '@'.", () => {
     const minMinusOne1PartE = random.email(3, 3, 2, 2, 2, 2)
     
-    regForm.fillIn(randomName, randomSurname, minMinusOne1PartE, randomPassword);
+    regForm.fillInNotValidLength(randomName, randomSurname, minMinusOne1PartE, randomPassword);
+    // Error about length
   });
 
   it.skip("Check registration by filling in the Email field with max+1 valid values before '@'.", () => {
     const maxPlusOne1PartE = random.email(65, 65, 32, 32, 16, 16)
     
-    regForm.fillIn(randomName, randomSurname, maxPlusOne1PartE, randomPassword);
+    regForm.fillInNotValidLength(randomName, randomSurname, maxPlusOne1PartE, randomPassword);
   });
 
   it.skip("Check registration by filling in the Email field with min-1 valid values after '@' and before last '.'.", () => {
     const minMinusOne2PartE = random.email(4, 4, 1, 1, 2, 2)
     
-    regForm.fillIn(randomName, randomSurname, minMinusOne2PartE, randomPassword);
+    regForm.fillInNotValidLength(randomName, randomSurname, minMinusOne2PartE, randomPassword);
   });
 
   it.skip("Check registration by filling in the Email field with max+1 valid values after '@' and before last '.'.", () => {
     const maxPlusOne2PartE = random.email(64, 64, 33, 33, 16, 16)
     
-    regForm.fillIn(randomName, randomSurname, maxPlusOne2PartE, randomPassword);
+    regForm.fillInNotValidLength(randomName, randomSurname, maxPlusOne2PartE, randomPassword);
   });
 
   it.skip("Check registration by filling in the Email field with min-1 valid values after last '.'.", () => {
     const minMinusOne3PartE = random.email(4, 4, 2, 2, 1, 1)
     
-    regForm.fillIn(randomName, randomSurname, minMinusOne3PartE, randomPassword);
+    regForm.fillInNotValidLength(randomName, randomSurname, minMinusOne3PartE, randomPassword);
   });
 
   it.skip("Check registration by filling in the Email field with max+1 valid values after last '.'.", () => {
     const maxPlusOne3PartE = random.email(64, 64, 32, 32, 17, 17)
     
-    regForm.fillIn(randomName, randomSurname, maxPlusOne3PartE, randomPassword);
+    regForm.fillInNotValidLength(randomName, randomSurname, maxPlusOne3PartE, randomPassword);
   });
 
   it.skip("Check registration by filling in the Password field with min-1 valid values.", () => {
     const minMinusOneP = random.password(7, 7)
     
-    regForm.fillIn(randomName, randomPassword, randomEmail, minMinusOneP);
+    regForm.fillInNotValidLength(randomName, randomPassword, randomEmail, minMinusOneP);
   });
 
   it.skip("Check registration by filling in the Password field with max+1 valid values.", () => {
     const maxPlusOneP = random.password(17, 17)
     
-    regForm.fillIn(randomName, randomSurname, randomEmail, maxPlusOneP);
+    regForm.fillInNotValidLength(randomName, randomSurname, randomEmail, maxPlusOneP);
   });
 
   it.skip("Check that Name field is required.", () => {
-    regForm.fillIn('', randomSurname, randomEmail, randomPassword);
+    regForm.fillInWihoutOneField('', randomSurname, randomEmail, randomPassword);
   });
 
   it.skip("Check that Email field is required.", () => {
-    regForm.fillIn(randomName, randomSurname, '', randomPassword);
+    regForm.fillInWihoutOneField(randomName, randomSurname, '', randomPassword);
   });
 
   it.skip("Check that Password field is required.", () => {
-    regForm.fillIn(randomName, randomSurname, randomEmail, '');
+    regForm.fillInWihoutOneField(randomName, randomSurname, randomEmail, '');
   });
 
   it.skip("Check that Surname field is not required.", () => {
@@ -153,10 +161,7 @@ describe("Registration form", () => {
 
   it.skip("Check", () => {
     /**
-     * Bugs 1 = Name field requires minimum 4 symbols;
-            2 = Name field requires maximum 25 symbols;
-            3 = Surname field accepts 1 symbol (min -1);
-            4 = Surname field accept 129 symbol (max +1)
+     *
             5 = Email field accepts 9 symbols (min -1)
                 5.1 Before '@' accepts 3 symbol (min -1)
                 5.2 Before last '.' accepts 1 symbol (min -1)
@@ -170,11 +175,14 @@ describe("Registration form", () => {
             9 = Surname is the required field
            10 = Email is not required field
            11 = Password is not required field
-           12 = Label suname instead of surname
+           13 = Name error
+           14 = Email error
+           15 = Password error
+           16 = SurName error
      */
     const ranName = random.nameSurname(4, 4), //2 - 128
     ranSurname = random.nameSurname(2, 2),
-    ranEmail = random.email(4, 4, 2, 2, 2, 2), // 4 - 64, 2 - 32, 2 - 16
+    ranEmail = random.email(4, 64, 2, 32, 1, 1), // 4 - 64, 2 - 32, 2 - 16
     ranPassword = random.password(8, 8) // 8 - 16
     regForm.fillInWihoutOneField(ranName, ranSurname, ranEmail, ranPassword);
   });
--- cypress/support/pageobj/form.js
+++ cypress/support/pageobj/form.js
@@ -6,9 +6,38 @@ class RegForm {
     cy.get("#password").type(password);
     cy.get('[type="submit"]').click();
     cy.get(".alert").then((el) => {
-      cy.log(el[0].innerText);
-      console.log(el[0].innerText);
       expect(el[0].innerText).includes(`Success! Hello: ${name} ${surname}`);
+    });
+  }
+
+  fillInNotValidLength(name, surname, email, password) {
+    cy.get("#name").type(name);
+    cy.get("#surname").type(surname);
+    cy.get("#email").type(email);
+    cy.get("#password").type(password);
+    cy.get('[type="submit"]').click();
+    cy.get(".alert").then((el) => {
+        if(name.length < 2 || name.length > 128){
+            expect(el[0].innerText).includes(`Error: Name Field length must be between 2 and 128`);
+        } else if(surname.length < 2 || surname.length > 128){
+            expect(el[0].innerText).includes(`Error: Surname Field length must be between 2 and 128`);
+        } else if(email.length < 10 || email.length > 114){
+            const addressSign = email.search('@'),
+                  lastPoint = email.lastIndexOf('.'),
+                  secondPart = lastPoint - addressSign,
+                  thirdPart = email.length - lastPoint
+                  if(addressSign < 4 || addressSign > 64){
+                    expect(el[0].innerText).includes(`Error: Before "@" length must be between 4 and 64`);
+                } else if (secondPart < 3 || secondPart > 33){
+                    expect(el[0].innerText).includes(`Error: After "@" and before "." length must be between 2 and 32`);
+                } else if(thirdPart < 3 || thirdPart > 17){
+                    expect(el[0].innerText).includes(`Error: Before last "." length must be between 2 and 16`);
+                } else{
+                    expect(el[0].innerText).includes(`Error: Email Field length must be between 10 and 114`);
+                }
+        } else if(password.length < 8 || password.length > 16){
+            expect(el[0].innerText).includes(`Error: Password Field length must be between 8 and 16`);
+        }
     });
   }
 
@@ -17,23 +46,36 @@ class RegForm {
       cy.get("#surname").type(surname);
       cy.get("#email").type(email);
       cy.get("#password").type(password);
-      cy.get('[type="submit"]').click();
     } else if (!surname) {
       cy.get("#name").type(name);
       cy.get("#email").type(email);
       cy.get("#password").type(password);
-      cy.get('[type="submit"]').click();
     } else if (!email) {
       cy.get("#name").type(name);
       cy.get("#surname").type(surname);
       cy.get("#password").type(password);
-      cy.get('[type="submit"]').click();
     } else if (!password) {
       cy.get("#name").type(name);
       cy.get("#surname").type(surname);
       cy.get("#email").type(email);
-      cy.get('[type="submit"]').click();
+    } else {
+      cy.get("#name").type(name);
+      cy.get("#surname").type(surname);
+      cy.get("#email").type(email);
+      cy.get("#password").type(password);
     }
+    cy.get('[type="submit"]').click();
+    cy.get(".alert").then((el) => {
+      if (!name) {
+        expect(el[0].innerText).includes(`Error: Name Field is Required`);
+      } else if (!email) {
+        expect(el[0].innerText).includes(`Error: Email Field is Required`);
+      } else if (!password) {
+        expect(el[0].innerText).includes(`Error: Password Field is Required`);
+      } else {
+        expect(el[0].innerText).includes(`Success! Hello: ${name} ${surname}`);
+      }
+    });
   }
 }
 
